f953294e89aff8cefb2b87e02b5b18af62433dd6
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-18 15:00:57 +0530

    express se throw hmform se data ko nikal kr console log me send krte h or ho jata h

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ const express = require('express')
 var path = require('path');
 const app = express()
 const port = 3000
+
+app.use(express.urlencoded())
+
 //express specific stuff
 app.use('/static', express.static('public'))
+
 //pug specific stuff
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
 //endpoints
 app.get('/', function (req, res) {
   const conttont = "helo and pug me hm html usg kr skte h ";
   const params = {"title":"pug html", "contant": conttont}
+  res.render('temp', params)
+})
+
+app.post('/',  (req, res)=> {
+  console.log(req.body)
+  const params = {"massage":"complite in forme"}
   res.render('temp', params)
 })
 
